cd6cb74c6db850de74a26098d502d5d6a938041e
SiWeir <siwei1368953@outlook.com>
2021-06-22 18:18:07 +0800

    分类内容complete

--- admin/src/views/CategoryEdit.vue
+++ admin/src/views/CategoryEdit.vue
@@ -3,6 +3,17 @@
     <h1>{{ id ? "编辑" : "新建" }}分类</h1>
     <!--    prevent 阻止事件-->
     <el-form label-width="120px" @submit.prevent="save">
+      <el-form-item label="上级分类">
+        <el-select v-model="model.parent">
+          <el-option
+            v-for="item in parents"
+            :key="item._id"
+            :label="item.name"
+            :value="item._id"
+          ></el-option>
+        </el-select>
+      </el-form-item>
+
       <el-form-item label="名称">
         <el-input v-model="model.name"></el-input>
       </el-form-item>
@@ -20,7 +31,7 @@ export default {
     id: {},
   },
   data() {
-    return { model: {} };
+    return { model: {}, parents: [] };
   },
   methods: {
     async save() {
@@ -42,8 +53,13 @@ export default {
       const res = await this.$http.get(`categories/${this.id}`);
       this.model = res.data;
     },
+    async fetchParents() {
+      const res = await this.$http.get(`categories`);
+      this.parents = res.data;
+    },
   },
   created() {
+    this.fetchParents();
     this.id && this.fetch();
   },
 };
--- server/models/Category.js
+++ server/models/Category.js
@@ -5,6 +5,7 @@ mongoose.set("useFindAndModify", false);
 const Schema = mongoose.Schema;
 const CategorySchema = new Schema({
   name: String,
+  parent: { type: mongoose.SchemaTypes.ObjectId, ref: "Category" },
 });
 
 module.exports = mongoose.model("Category", CategorySchema);
